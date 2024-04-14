4052c2223e5deea667ea124253bc70464bc94ae1
KonstantinMuravyev <k.muravyev2016@yandex.ru>
2024-04-15 05:38:47 +1000

    add App

---
+++ src/main/java/app/service/UserServiceImpl.java
@@ -0,0 +1,55 @@
+package app.service;
+
+
+import app.dao.UserDao;
+
+import app.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public User getUserById(long id) {
+        return userDao.getUserById(id);
+    }
+
+    @Override
+    @Transactional
+    public void updateUser(User user) {
+        userDao.updateUser(user);
+    }
+
+    @Override
+    @Transactional
+    public void removeUserById(long id) {
+        userDao.removeUserById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        userDao.saveUser(user);
+    }
+}
